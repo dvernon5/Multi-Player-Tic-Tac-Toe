ef70567bde7343f289a7a37e29b34b690d815b14
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2023-10-25 12:43:34 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,288 +1,187 @@
-#include<iostream>
-#include<string>
-using namespace std;
+#include <iostream>
+#include <string>
 
-const int ROW_AND_COLUMN = 3;
+const int ROWS_AND_COLUMNS = 3;
+void DisplayGameBoard(const char[][ROWS_AND_COLUMNS]);
+void Player(char[][ROWS_AND_COLUMNS], const char);
+int InputValidation(int, const int, const int);
+bool CheckIfWinner(const char[][ROWS_AND_COLUMNS], const char, std::string&);
+bool CheckIfWinnerAcross(const char[][ROWS_AND_COLUMNS], const char, std::string&);
+bool CheckIfWinnerDown(const char[][ROWS_AND_COLUMNS], const char, std::string&);
+bool CheckIfWinnerLeftToRightDiagonal(const char[][ROWS_AND_COLUMNS], const char, std::string&);
+bool CheckIfWinnerRightToLeftDiagonal(const char[][ROWS_AND_COLUMNS], const char, std::string&);
 
-void displayGameBoard(char array[][ROW_AND_COLUMN]);
-
-void player(char array[][ROW_AND_COLUMN], char);
-
-bool checkIfWinnerAcross(const char array[][ROW_AND_COLUMN], const char, string&);
-
-bool checkIfWinnerDown(const char array[][ROW_AND_COLUMN], const char, string&);
-
-bool checkIfWinnerLeftToRightDiagonal(const char array[][ROW_AND_COLUMN], const char, string&);
-
-bool checkIfWinnerRightToLeftDiagonal(const char array[][ROW_AND_COLUMN], const char, string&);
-
-bool checkIfWinner(const char array[][ROW_AND_COLUMN], const char, string&);
-
-int inputValidate(int, int, int);
-
-int main(void)
-{
-    char gameBoard[][ROW_AND_COLUMN] = { { '*', '*', '*' },
-                                         { '*', '*', '*' },
-                                         { '*', '*', '*' } };
-    string gameWinner = "";
-    
-    displayGameBoard(gameBoard);
-    
-    player(gameBoard, 'X');
-    displayGameBoard(gameBoard);
-    
-    for (int i = 0; i < 4; i++)
-    {
-        player(gameBoard, 'O');
-        displayGameBoard(gameBoard);
-        
-        if (checkIfWinner(gameBoard, 'O', gameWinner) == true)
-        {
-            break;
-        }
-        
-        player(gameBoard, 'X');
-        displayGameBoard(gameBoard);
-        
-        if (checkIfWinner(gameBoard, 'X', gameWinner) == true)
-        {
-            break;
-        }
+int main(void) {
+  char game_board[ROWS_AND_COLUMNS][ROWS_AND_COLUMNS] = {
+    { '*', '*', '*'},
+    { '*', '*', '*'},
+    { '*', '*', '*'}
+  };
+  std::string game_winner = "";
+  DisplayGameBoard(game_board);
+  Player(game_board, 'X');
+  DisplayGameBoard(game_board);
+  for (size_t itr = 0; itr < 4; ++itr) {
+    Player(game_board, 'O');
+    DisplayGameBoard(game_board);
+    if (CheckIfWinner(game_board, 'O', game_winner) == true) {
+      break;
     }
-    
-    cout << "Game winner: " << gameWinner << endl;
-    
-    return 0;
+    Player(game_board, 'X');
+    DisplayGameBoard(game_board);
+    if (CheckIfWinner(game_board, 'X', game_winner) == true) {
+      break;
+    }
+  }
+  
+  std::cout << "Game winner: " << game_winner << std::endl;
+  
+  return 0;
 }
 
-void displayGameBoard(char array[][ROW_AND_COLUMN])
-{
-    cout << endl;
-    
-    for (int row = 0; row < ROW_AND_COLUMN; row++)
-    {
-        for (int column = 0; column < ROW_AND_COLUMN; column++)
-        {
-            cout << array[row][column] << " ";
-        }
-        cout << endl;
+void DisplayGameBoard(const char game_board[][ROWS_AND_COLUMNS]) {
+  std::cout << std::endl;
+  for (size_t row = 0; row < ROWS_AND_COLUMNS; ++row) {
+    for (size_t column = 0; column < ROWS_AND_COLUMNS; ++column) {
+      std::cout << game_board[row][column] << " ";
     }
-    
-    cout << endl;
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
 }
 
-void player(char array[][ROW_AND_COLUMN], char letter)
-{
-    cout << "Select a location to place an " << letter << ": " << endl;
-    
-    int row = 0, column = 0;
-    bool spaceOccupied = false;
-    
-    do
-    {
-        cout << "Which row? (Enter a number between 1 and 3): ";
-        row = inputValidate(row, 1, 3);
-        
-        cout << "Which column? (Enter a number between 1 and 3): ";
-        column = inputValidate(column, 1, 3);
-        
-        if (array[(row - 1)][(column - 1)] == '*')
-        {
-            array[(row - 1)][(column - 1)] = letter;
-            spaceOccupied = true;
-        }
-        else
-        {
-            cout << "Sorry! spot is taken." << endl;
-            spaceOccupied = false;
-        }
-    } while (spaceOccupied == false);
+void Player(char game_board[][ROWS_AND_COLUMNS], const char letter) {
+  std::cout << "Select a location to place " << letter << std::endl;
+  int row = 0;
+  int column = 0;
+  bool space_occupied = false;
+  do {
+    std::cout << "Enter a row (Enter a number between 1 and 3): ";
+    row = InputValidation(row, 1, 3);
+    std::cout << "Enter a column: (Enter a number between 1 and 3): ";
+    column = InputValidation(column, 1, 3);
+    if (game_board[row - 1][column - 1] == '*') {
+      game_board[row - 1][column - 1] = letter;
+      space_occupied = true;
+    } else {
+      std::cerr << "Sorry space is taken. Please try again." << std::endl;;
+    }
+  } while (space_occupied == false);
 }
 
-int inputValidate(int userInput, int lowest, int highest)
-{
-    while (!(cin >> userInput) || (userInput < lowest) || (userInput > highest))
-    {
-        cout << "Error. Enter number between " << lowest << " to " << highest << ": ";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-    
-    return userInput;
+int InputValidation(int user_input, const int lowest, const int highest) {
+  while (!(std::cin >> user_input) || (user_input < lowest) || (user_input > highest)) {
+    std::cerr << "Invalid! Enter a number between " << lowest << " and " << highest << ": ";
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  }
+  
+  return user_input;
 }
 
-bool checkIfWinner(const char array[][ROW_AND_COLUMN], const char letter, string& gameWinner)
-{
-    bool winner = false;
-    
-    if (checkIfWinnerAcross(array, letter, gameWinner) == true)
-    {
-        winner = true;
-    }
-    else if (checkIfWinnerDown(array, letter, gameWinner) == true)
-    {
-        winner = true;
-    }
-    else if (checkIfWinnerLeftToRightDiagonal(array, letter, gameWinner) == true)
-    {
-        winner = true;
-    }
-    else if (checkIfWinnerRightToLeftDiagonal(array, letter, gameWinner) == true)
-    {
-        winner = true;
-    }
-    else
-    {
-        gameWinner = "TIE";
-        winner = false;
-    }
-    
-    return winner;
+bool CheckIfWinner(const char game_board[][ROWS_AND_COLUMNS], const char letter, std::string& game_winner) {
+  if (CheckIfWinnerAcross(game_board, letter, game_winner) == true) {
+    return true;
+  } else if (CheckIfWinnerDown(game_board, letter, game_winner) == true) {
+    return true;
+  } else if (CheckIfWinnerLeftToRightDiagonal(game_board, letter, game_winner) == true) {
+    return true;
+  } else if (CheckIfWinnerRightToLeftDiagonal(game_board, letter, game_winner) == true) {
+    return true;
+  } else {
+    game_winner = "TIE";
+  }
+  
+  return false;
 }
 
-bool checkIfWinnerAcross(const char array[][ROW_AND_COLUMN], const char letter, string& gameWinner)
-{
-    int letterAcross = 0;
-    bool winner = false;
-    
-    for (int row = 0; row < ROW_AND_COLUMN; row++)
-    {
-        for (int column = 0; column < ROW_AND_COLUMN; column++)
-        {
-            if (array[row][column] == letter)
-            {
-                letterAcross++;
-            }
-        }
-        
-        if (letterAcross == 3)
-        {
-            if (letter == 'X')
-            {
-                gameWinner = "Player1";
-            }
-            else if (letter == 'O')
-            {
-                gameWinner = "Player2";
-            }
-            
-            winner = true;
-            break;
-        }
-        else
-        {
-            winner = false;
-            letterAcross = 0;
-        }
+bool CheckIfWinnerAcross(const char game_board[][ROWS_AND_COLUMNS], const char letter, std::string& game_winner) {
+  int letter_across = 0;
+  bool winner = false;
+  for (size_t row = 0; row < ROWS_AND_COLUMNS; ++row) {
+    for (size_t column = 0; column < ROWS_AND_COLUMNS; ++column) {
+      if (game_board[row][column] == letter) {
+        ++letter_across;
+      }
     }
-    
-    return winner;
+    if (letter_across == 3) {
+      if (letter == 'X') {
+        game_winner = "Player1";
+      } else if (letter == 'O') {
+        game_winner = "Player2";
+      }
+      winner = true;
+      break;
+    } else {
+      letter_across = 0;
+    }
+  }
+  
+  return winner;
 }
 
-bool checkIfWinnerDown(const char array[][ROW_AND_COLUMN], const char letter, string& gameWinner)
-{
-    int letterDown = 0;
-    bool winner = false;
-    
-    for (int row = 0; row < ROW_AND_COLUMN; row++)
-    {
-        for (int column = 0; column < ROW_AND_COLUMN; column++)
-        {
-            if (array[row][column] == letter)
-            {
-                letterDown++;
-            }
-        }
-        
-        if (letterDown == 3)
-        {
-            if (letter == 'X')
-            {
-                gameWinner = "Player1";
-            }
-            else if (letter == 'O')
-            {
-                gameWinner = "Player2";
-            }
-            
-            winner = true;
-            break;
-        }
-        else
-        {
-            winner = false;
-            letterDown = 0;
-        }
+bool CheckIfWinnerDown(const char game_board[][ROWS_AND_COLUMNS], const char letter, std::string& game_winner) {
+  int letter_down = 0;
+  bool winner = false;
+  for (size_t row = 0; row < ROWS_AND_COLUMNS; ++row) {
+    for (size_t column = 0; column < ROWS_AND_COLUMNS; ++column) {
+      if (game_board[column][row] == letter) {
+        ++letter_down;
+      }
     }
-    
-    return winner;
+    if (letter_down == 3) {
+      if (letter == 'X') {
+        game_winner = "Player1";
+      } else if (letter == 'O') {
+        game_winner = "Player2";
+      }
+      winner = true;
+      break;
+    } else {
+      letter_down = 0;
+    }
+  }
+  
+  return winner;
 }
 
-bool checkIfWinnerLeftToRightDiagonal(const char array[][ROW_AND_COLUMN], const char letter, string& gameWinner)
-{
-    int letterDiagonal = 0;
-    bool winner = false;
-    
-    for (int i = 0; i < ROW_AND_COLUMN; i++)
-    {
-        if (array[i][i] == letter)
-        {
-            letterDiagonal++;
-        }
+bool CheckIfWinnerLeftToRightDiagonal(const char game_board[][ROWS_AND_COLUMNS], const char letter, std::string& game_winner) {
+  int letter_diagonal = 0;
+  bool winner = false;
+  for (size_t current_index = 0; current_index < ROWS_AND_COLUMNS; ++current_index) {
+    if (game_board[current_index][current_index] == letter) {
+      ++letter_diagonal;
     }
-    
-    if (letterDiagonal == 3)
-    {
-        if (letter == 'X')
-        {
-            gameWinner = "Player1";
-        }
-        else if (letter == 'O')
-        {
-            gameWinner = "Player2";
-        }
-        
-        winner = true;
+  }
+  if (letter_diagonal == 3) {
+    if (letter == 'X') {
+      game_winner = "Player1";
+    } else if (letter == 'O') {
+      game_winner = "Player2";
     }
-    else
-    {
-        winner = false;
-    }
-    
-    return winner;
+    winner = true;
+  }
+  
+  return winner;
 }
 
-bool checkIfWinnerRightToLeftDiagonal(const char array[][ROW_AND_COLUMN], const char letter, string& gameWinner)
-{
-    int letterDiagonal = 0;
-    int winner = false;
-    
-    for (int row = 0, column = ROW_AND_COLUMN - 1; row < ROW_AND_COLUMN; row++, column--)
-    {
-        if (array[row][column] == letter)
-        {
-            letterDiagonal++;
-        }
+bool CheckIfWinnerRightToLeftDiagonal(const char game_board[][ROWS_AND_COLUMNS], const char letter, std::string& game_winner) {
+  int letter_diagonal = 0;
+  bool winner = false;
+  for (size_t row = 0; row < ROWS_AND_COLUMNS; ++row) {
+    auto column = ROWS_AND_COLUMNS - row - 1;
+    if (game_board[row][column] == letter) {
+      ++letter_diagonal;
     }
-    
-    if (letterDiagonal == 3)
-    {
-        if (letter == 'X')
-        {
-            gameWinner = "Player1";
-        }
-        else if (letter == 'O')
-        {
-            gameWinner = "Player2";
-        }
-        
-        winner = true;
+  }
+  if (letter_diagonal == 3) {
+    if (letter == 'X') {
+      game_winner = "Player1";
+    } else if (letter == 'O') {
+      game_winner = "Player2";
     }
-    else
-    {
-        winner = false;
-    }
-    
-    return winner;
+    winner = true;
+  }
+  
+  return winner;
 }
