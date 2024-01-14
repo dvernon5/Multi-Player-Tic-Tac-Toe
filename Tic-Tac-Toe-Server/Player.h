497f04b47212f46f36e4f44636e0b291e19e8d07
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2024-01-14 12:57:56 -0800

    Create Player.h
    Holds variables for row and column numbers.

---
+++ Tic-Tac-Toe-Server/Player.h
@@ -0,0 +1,17 @@
+#ifndef Player_h
+#define Player_h
+
+/* ------------------------------------------------------------------------
+ * STRUCT NAME: Player
+ * ------------------------------------------------------------------------
+ * @brief Hold variables for row and column number.
+ *
+ * The Player struct is used to allow user to enter a row and column number
+ * to make a move on the game board.
+ * ------------------------------------------------------------------------
+ */
+struct Player {
+  int row;
+  int column;
+};
+#endif /* Player_h */
